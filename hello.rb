9d639f18ce71cbbff5471fa11f5cd183ec3660c4
gregory <public+github@gregory.io>
2016-04-06 14:27:24 -0700

    print errors when querying store API

--- hello.rb
+++ hello.rb
@@ -79,7 +79,11 @@ get '/' do
 
   @bc_api_url = bc_api_url
   @client_id = bc_client_id
-  @products = JSON.pretty_generate(@store.bc_api.products)
+  begin
+    @products = JSON.pretty_generate(@store.bc_api.products)
+  rescue => e
+    return render_error(e.message)
+  end
 
   erb :index
 end
